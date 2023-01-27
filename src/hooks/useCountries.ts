d56dfadf58625ca2905c9f993d1755a3bd6b0e67
Firman Justisio Lestari <jusles363@gmail.com>
2023-01-27 23:18:23 +0700

    feat: add filter on hook

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -7,8 +7,12 @@ interface CountryType {
 }
 
 const useCountries = () => {
-    const {data, ...res} = useSWR<CountryType[]>('https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/option_area', fetcher)
-    return {data, ...res}
+    const {data, ...res} = useSWR<any[]>('https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/option_area', fetcher)
+    const newData: CountryType[] | undefined = data?.filter(data => {
+        if (data.province && data.city) return false
+        return true
+    })
+    return {data: newData, ...res}
 }
 
 export type {

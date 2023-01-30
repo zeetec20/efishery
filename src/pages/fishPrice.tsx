2b1e03855203f61fe05d87086415166088b87b21
Firman Justisio Lestari <jusles363@gmail.com>
2023-01-30 20:46:22 +0700

    feat: title on specific page

--- src/pages/fishPrice.tsx
+++ src/pages/fishPrice.tsx
@@ -1,12 +1,16 @@
 import { TableFishPrice } from "src/components/fishPrice"
 import useFishs from "src/hooks/useFishs"
 import 'src/styles/pages/fishPrice/fishPrice.scss'
+import { Helmet } from 'react-helmet'
 
 const FishPrice = () => {
     const { data } = useFishs()
 
     return (
         <div className="fish-price">
+            <Helmet>
+                <title>Efishman - Harga Ikan</title>
+            </Helmet>
             <TableFishPrice fishs={data} />
         </div>
     )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import useFishs from 'src/hooks/useFishs'
 import { getGroupFishOfCity, getRankFish } from 'src/services/fish'
 import 'src/styles/pages/home/home.scss'
 import { useDraggable } from 'react-use-draggable-scroll'
+import { Helmet } from 'react-helmet'
 
 const Home = () => {
     const { data } = useFishs()
@@ -23,6 +24,9 @@ const Home = () => {
 
     return (
         <div className='home'>
+            <Helmet>
+                <title>Efishman</title>
+            </Helmet>
             <div className="wrap-card-city" {...events} ref={ref}>
                 <ListCardCountry />
             </div>
